4095e1a733cd32013bdd0fe58621db20beb3ed1e
Agrames <davidagugharam@gmail.com>
2026-04-03 01:50:58 +0100

    Initial commit

---
+++ src/main/java/com/mycompany/com2007groupcoursework/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.com2007groupcoursework;
+
+/**
+ *
+ * @author HP
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
