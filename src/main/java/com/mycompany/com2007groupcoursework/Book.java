21504b093946043262546ad73808381b9621f2a9
cu23aan <cu23aan@herts.ac.uk>
2026-04-07 13:14:02 +0100

    i added book.java

---
+++ src/main/java/com/mycompany/com2007groupcoursework/Book.java
@@ -0,0 +1,26 @@
+package com.mycompany.com2007groupcoursework;
+
+public class Book extends Item {
+private String author;
+private String isbn;
+
+public Book(String title, String author, Member donatedBy,
+String language, String isbn) {
+super(title, language, donatedBy);
+this.author = author;
+this.isbn = isbn;
+}
+
+public String getAuthor() { return author; }
+public void setAuthor(String author) { this.author = author; }
+
+public String getIsbn() { return isbn; }
+public void setIsbn(String isbn) { this.isbn = isbn; }
+
+@Override
+public String toString() {
+return "Book: " + title + " by " + author +
+" | ISBN: " + isbn + " | Lang: " + language +
+" | Available: " + isAvailable();
+}
+}
